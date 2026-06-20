7d384ddfe88696ba58a3bc510b7e9f85d1212555
amaan123khan <amaankhan72253@gmail.com>
2026-06-20 12:06:44 +0530

    Updated navbar and admin panel UI

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="" />
-      <button>Login</button>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='h-20 w-auto' src={assets.logo} alt="" />
+      <button className='bg-gray-600 text-white px-6 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
 
     </div>
   )
